6a6d0ff2302d9bd5283aa7bcd078b01675cb1e82
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-20 18:05:24 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+attr_accessor :name, :artist, :genre
+
+
+@@count = 0
+
+@@artists = []
+
+@@genres = []
+
+@@genre_count = {}
+
+@@artist_count = {}
+
+def initialize(name, artist, genre)
+@name = name
+@artist = artist
+@genre = genre
+@@count += 1
+end
+
+def count
+  @@count
+end
+
+
+
+end
